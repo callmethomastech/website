44aa4d2d563578dc177cb6eac24991446035831b
Thomas Henderson <elementplayzgames@gmail.com>
2023-01-06 05:40:32 -0600

    finished basic portfolio layout

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 function PortfolioItem({ imgUrl, title, stack, link }) {
     return (
-        <div>
+        <div className="border-2 border-stone-900 rounded-md overflow-hidden">
             <img
                 src={imgUrl}
-                alt="project item"
+                alt="portfolio item"
                 className="w-full h-36 md:h-48 object-cover cursor-pointer"
             />
             <div className="w-full p-4">
-                <h3>{title}</h3>
-                <p>
+                <h3 className="text-md md:text-lg mb-2 md:mb-3 font-semibold"><a href={link} target="_blank">{title}</a></h3>
+                <p>Stack:</p>
+                <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm">
                     {stack.map(item => (
-                        <span>
+                        <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-2xl">
                             {item}
                         </span>
                     ))}
