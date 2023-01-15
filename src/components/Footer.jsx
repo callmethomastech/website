9d2084ddf040e70daed87ff4d7c8075decdbdb65
Thomas Henderson <elementplayzgames@gmail.com>
2023-01-15 04:03:08 -0600

    fixed footer logos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
     return (
         <div className="mt-10">
             <div className="flex flex-row gap-2 justify-center">
-                <a href ="https://youtube.com/c/FADEDElement" target="_blank"><img src="public/assets/general/youtube_logo.png" /></a>
-                <a href ="https://github.com/FADEDElement" target="_blank"><img src="public/assets/general/github_logo.png" /></a>
-                <a href ="https://discord.com/invite/XdxwyrE" target="_blank"><img src="public/assets/general/discord_logo.png" /></a>
+                <a href ="https://youtube.com/c/FADEDElement" target="_blank"><img src="../public/assets/general/youtube_logo.png" /></a>
+                <a href ="https://github.com/FADEDElement" target="_blank"><img src="../public/assets/general/github_logo.png" /></a>
+                <a href ="https://discord.com/invite/XdxwyrE" target="_blank"><img src="../public/assets/general/discord_logo.png" /></a>
                 
             </div>
             <p className="text-sm mt-1 opacity-50 text-center">
