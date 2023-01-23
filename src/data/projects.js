12f0dbd06dcac452ff299565a0a9609ffc5f3dc7
Thomas Henderson <elementplayzgames@gmail.com>
2023-01-23 00:59:11 -0600

    fixed picture display issue

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,30 +1,30 @@
 export default [
     {
-        imgUrl: "public/assets/project_thumbnails/elebot.png",
+        imgUrl: "assets/project_thumbnails/elebot.png",
         title: "EleBot",
         stack: ["Python"],
         link: "https://github.com/FADEDElement/EleBot",
     }, 
     {
-        imgUrl: "public/assets/project_thumbnails/website.png",
+        imgUrl: "assets/project_thumbnails/website.png",
         title: "My Website",
         stack: ["React", "Tailwind", "Vite"],
         link: "https://github.com/FADEDElement/website",
     },
     {
-        imgUrl: "public/assets/project_thumbnails/tictactoe.png",
+        imgUrl: "assets/project_thumbnails/tictactoe.png",
         title: "Tic Tac Toe",
         stack: ["Python"],
         link: "https://github.com/FADEDElement/TicTacToe",
     }, 
     {
-        imgUrl: "public/assets/project_thumbnails/tntmode.png",
+        imgUrl: "assets/project_thumbnails/tntmode.png",
         title: "TNT Mode",
         stack: ["MCFunction"],
         link: "https://github.com/FADEDElement/TNTMode",
     }, 
     {
-        imgUrl: "public/assets/project_thumbnails/fbla-cst.png",
+        imgUrl: "assets/project_thumbnails/fbla-cst.png",
         title: "FBLA CST",
         stack: ["Java", "SQL"],
         link: "https://github.com/FADEDElement/Community-Service-Tracker",
