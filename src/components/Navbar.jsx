e8fa9a03c4230b395db301c0faac10089ba64498
Thomas Henderson <elementplayzgames@gmail.com>
2023-01-31 04:47:10 -0600

    added smooth scroll to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,9 @@ function Navbar() {
         <div className="flex flex-col p-4 items-center bg-davys-grey w-full fixed md:flex-row z-10">
             <p className="text-xl grow"><span className="text-blue-green">&lt;/</span>fadedelement<span className="text-blue-green">&gt;</span></p>
             <div className="flex flex-row gap-4">
-                <a href="/" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Home</a>
-                <a href="#portfolio" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Portfolio</a>
-                <a href="#timeline" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Timeline</a>
+                <button onClick={() => document.getElementById("home").scrollIntoView({behavior: "smooth"})} className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Home</button>
+                <button onClick={() => document.getElementById("portfolio").scrollIntoView({behavior: "smooth"})} className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Portfolio</button>
+                <button onClick={() => document.getElementById("timeline").scrollIntoView({behavior: "smooth"})} className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Timeline</button>
             </div>
         </div>
     )
