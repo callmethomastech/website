7293eb28d44211443215388cb4b6bb7c29e2a5b7
Thomas Henderson <elementplayzgames@gmail.com>
2023-01-31 04:27:00 -0600

    removed blog -for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,6 @@ function Navbar() {
             <p className="text-xl grow"><span className="text-blue-green">&lt;/</span>fadedelement<span className="text-blue-green">&gt;</span></p>
             <div className="flex flex-row gap-4">
                 <a href="/" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Home</a>
-                <a href="/blog" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Blog</a>
-                <span>·</span>
                 <a href="#portfolio" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Portfolio</a>
                 <a href="#timeline" className="relative after:absolute after:content-[''] after:bg-blue-green after:h-[3px] after:w-[0%] after:left-0 after:-bottom-[1px] after:rounded-xl after:duration-300 after:hover:w-[100%]">Timeline</a>
             </div>
